99d906039a157eb2ea334d7575f10072939752df
Brandon Pittman <brandon@brandonpittman.net>
2019-10-18 15:37:14 +0900

    Improve error handling

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const chalk = require('chalk');
 const override = require('./lib/override');
 const scaffold = require('./lib/scaffold');
 const fresh = require('./lib/fresh');
 const pascalcase = require('pascalcase');
 
 module.exports = ({input, flags, showHelp}) => {
-	const command = input[0];
+	const COMMANDS = [
+		'scaffold',
+		'override',
+		'fresh'
+	];
 
-	switch (command) {
+	const COMMAND = input[0];
+
+	if (!COMMANDS.includes(COMMAND)) {
+		console.log(chalk.bold.red(`\n${COMMAND} is not a recognized command.\n`));
+		console.log(chalk.underline('Available commands:'));
+		for (let command of COMMANDS) {
+			console.log(`${command}`);
+		}
+
+		process.exit(126);
+	}
+
+	switch (COMMAND) {
 		case 'scaffold':
 			scaffold(
 				pascalcase(flags.type),
