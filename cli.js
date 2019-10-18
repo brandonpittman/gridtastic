76b9b86789a6710360f2542ade0c7a385e6cfaea
Brandon Pittman <brandon@brandonpittman.net>
2019-10-18 13:23:14 +0900

    improve file structure

--- cli.js
+++ cli.js
@@ -2,51 +2,27 @@
 
 'use strict';
 
-const meow = require('meow');
 const pascalcase = require('pascalcase');
 
+const parser = require('./lib/parser');
 const override = require('./lib/override');
-const copyTemplate = require('./lib/copyTemplate');
+const scaffold = require('./lib/scaffold');
 const fresh = require('./lib/fresh');
 
-const cli = meow(`
-Usage
-    $ gridsome-helpers
-
-    override html|vue                           Override filetype
-    scaffold -t TYPE -n NAME                    Scaffold out a new file
-    fresh                                       Delete Gridsome boilerplate pages and folder-specific README.md files
-
-  Options 
-    --type, -t  Page|Template|Layout|Component  Filetype to be scaffolded
-    --name, -n  SomeFilename                    Filename to be used (will be pascal cased by CLI)
-    --help, -h                                  Show help
-`, {
-	flags: {
-		help: {
-			alias: 'h',
-			type: 'boolean'
-		},
-		type: {
-			type: 'string',
-			alias: 't'
-		},
-		name: {
-			type: 'string',
-			alias: 'n'
-		}
-	}
-});
-
-const command = cli.input[0];
+const cli = parser;
+const {input, flags, showHelp} = cli;
+const command = input[0];
 
 switch (command) {
 	case 'scaffold':
-		copyTemplate(pascalcase(cli.flags.type), pascalcase(cli.flags.name));
+		scaffold(
+			pascalcase(flags.type),
+			pascalcase(flags.name)
+		);
 		break;
 
 	case 'override':
-		override(cli.input[1]);
+		override(input[1]);
 		break;
 
 	case 'fresh':
@@ -54,5 +30,5 @@ switch (command) {
 		break;
 
 	default:
-		cli.showHelp();
+		showHelp();
 }
