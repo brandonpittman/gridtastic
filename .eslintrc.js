b1156d659082b82b785240d5f7fca8c04f415039
Brandon Pittman <brandon@brandonpittman.net>
2020-02-14 23:00:07 +0900

    Revert to @zeit/arg

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
+  "env": {
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
   "extends": ["xo"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    }
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+  }
 };
--- main.ts
+++ main.ts
@@ -1,43 +1,30 @@
-// Import chalk from 'chalk';
-// import {version} from './package.json';
-// import override from './lib/override';
-// import scaffold from './lib/scaffold';
-// import fresh from './lib/fresh';
-// import degit from './lib/degit';
-// import {log} from 'console';
-//
-// export default function ({args, help}) {
-// 	const COMMANDS = [
-// 		'init',
-// 		'scaffold',
-// 		'override',
-// 		'fresh'
-// 	];
-//
-// 	switch (COMMAND) {
-// 		case 'init':
-// 			degit(args);
-// 			break;
-//
-// 		case 'scaffold':
-// 			scaffold(args);
-// 			break;
-//
-// 		case 'override':
-// 			override(args);
-// 			break;
-//
-// 		case 'fresh':
-// 			fresh();
-// 			break;
-//
-// 		default:
-// 			log(chalk.bold.red(`\n${COMMAND} is not a recognized command.\n`));
-// 			log(chalk.underline('Available commands:'));
-// 			for (const command of COMMANDS) {
-// 				log(`${command}`);
-// 			}
-//
-// 			process.exit(126);
-// 	}
-// }
+import chalk from 'chalk';
+import {version} from './package.json';
+import override from './lib/override';
+import scaffold from './lib/scaffold';
+import fresh from './lib/fresh';
+import degit from './lib/degit';
+import {log} from 'console';
+
+export default function ({help, args}) {
+  if (args['--version']) {
+    log(version);
+  }
+
+  if (args['--help']) {
+    log(help);
+  }
+
+  const commands = {
+    init: degit,
+    scaffold: scaffold,
+    override: override,
+    fresh: fresh
+  };
+
+  const command = commands[args._[0]];
+
+  if (command) {
+    command();
+  }
+}
