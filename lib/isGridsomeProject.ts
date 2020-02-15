4e83c568100f839403b79ef96e95b3b4c30ba806
Brandon Pittman <brandon@brandonpittman.net>
2020-02-15 10:38:21 +0900

    Revert to @zeit/arg

--- lib/isGridsomeProject.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import chalk from 'chalk';
-
-export default () => {
-	if (!fs.existsSync('./gridsome.config.js')) {
-		console.log(chalk.red('Not in root of a Gridsome project!'));
-		process.exit(126);
-	}
-};
