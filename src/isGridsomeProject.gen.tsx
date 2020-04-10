b00468fd69c495c62971620a7aa72f6a85d04c04
Brandon Pittman <github@brandonpittman.net>
2020-04-10 11:55:21 +0900

    Create typed isGridsomeProject

---
+++ src/isGridsomeProject.gen.tsx
@@ -0,0 +1,10 @@
+/* TypeScript file generated from isGridsomeProject.re by genType. */
+/* eslint-disable import/first */
+
+
+// tslint:disable-next-line:no-var-requires
+const isGridsomeProjectBS = require('./isGridsomeProject.bs');
+
+export const $$default: () => void = isGridsomeProjectBS.default;
+
+export default $$default;
--- src/isGridsomeProject.re
+++ src/isGridsomeProject.re
@@ -1,6 +1,7 @@
+[@genType]
 let default = () => {
   if (!Node_fs.existsSync("./gridsome.config.js")) {
     Js.Console.error("Not in the root of a Gridsome project!")
     Node_process.exit(1)
   }
-}
+};
