5e1e175d95671a3c7bdbac18f9f25410c5eafcfe
Brandon Pittman <github@brandonpittman.net>
2020-04-10 11:06:29 +0900

    Convert isGridsomeProject

---
+++ src/isGridsomeProject.re
@@ -0,0 +1,6 @@
+let default = () => {
+  if (!Node_fs.existsSync("./gridsome.config.js")) {
+    Js.Console.error("Not in the root of a Gridsome project!")
+    Node_process.exit(1)
+  }
+}
--- src/isGridsomeProject.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import chalk from 'chalk';
-
-export default (): void => {
-	if (!fs.existsSync('./gridsome.config.js')) {
-		console.log(chalk.red('Not in root of a Gridsome project!'));
-		process.exit(1);
-	}
-};
